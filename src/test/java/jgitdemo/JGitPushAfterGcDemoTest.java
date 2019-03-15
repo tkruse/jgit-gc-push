efdc46f75a657294ad748ea7675235f0cfd726a1
Thibault Kruse <tkruse@indeed.com>
2019-03-15 14:28:11 +0900

    Successful clone, modify, push

---
+++ src/test/java/jgitdemo/JGitPushAfterGcDemoTest.java
@@ -0,0 +1,133 @@
+package jgitdemo;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.TextProgressMonitor;
+import org.eclipse.jgit.transport.PushResult;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * This JUnit test assumes that a git repo exists on github, and that valid github credentials are given in a property file.
+ */
+public class JGitPushAfterGcDemoTest {
+
+    private static final Logger LOGGER = Logger.getLogger(JGitPushAfterGcDemoTest.class);
+    private static final TextProgressMonitor PROGRESS_MONITOR = new TextProgressMonitor(new LoggerPrintWriter(LOGGER, Level.DEBUG));
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void reproducePushSlowdownAfterGc() throws Exception {
+        final Properties properties = readProperties("gitlab");
+        final String gitUsername = properties.getProperty("user");
+        final String gitPassword = properties.getProperty("password");
+        final UsernamePasswordCredentialsProvider credProvider = new UsernamePasswordCredentialsProvider(gitUsername, gitPassword);
+
+//        final Properties properties = readProperties("github");
+//        final String githubProject = "linux";
+        //final String remoteURL = "https://github.com/" + gitUsername + "/" + githubProject;
+
+        final String githubProject = "proctor-data.git";
+        final String remoteURL = "https://code.corp.indeed.com/" + gitUsername + "/" + githubProject;
+
+
+        // choose beween cleaned-up folder and fixed folder
+//        final File localFolder = folder.newFolder("react");
+        final File localFolder = new File("/tmp/" + githubProject);
+
+        LOGGER.debug("Cloning from " + remoteURL + " as " + gitUsername + " to " + localFolder);
+        final Git git = cloneRepository(remoteURL, localFolder, credProvider);
+
+        /* *********** Call to reproduce later git push delay **********/
+        LOGGER.debug("Call GC");
+        git.gc().setProgressMonitor(PROGRESS_MONITOR).call();
+
+
+
+        LOGGER.debug("Modify, add and commit");
+        modifyAndCommitReadme(gitUsername, gitPassword, localFolder, git);
+
+        LOGGER.debug("Push");
+        final long start = System.currentTimeMillis();
+        // push
+        final Iterable<PushResult> pushResults = git.push()
+                .setProgressMonitor(PROGRESS_MONITOR)
+                .setCredentialsProvider(credProvider)
+                .call();
+        LOGGER.info("Push took " + (System.currentTimeMillis() - start));
+        LOGGER.info(pushResults);
+    }
+
+    private void modifyAndCommitReadme(final String githubUsername, final String githubPassword, final File localFolder, final Git git) throws IOException, GitAPIException {
+        final File readmeFile = new File(localFolder, "README.md");
+
+        // modify README
+        try (FileWriter fr = new FileWriter(readmeFile, true)) {
+            fr.write(System.currentTimeMillis() + "\n");
+        }
+
+        git.add()
+                .addFilepattern(readmeFile.getAbsolutePath())
+                .call();
+        git.commit()
+                .setCommitter(githubUsername, githubUsername)
+                .setAuthor(githubUsername, githubPassword)
+                .setMessage("commit " + System.currentTimeMillis())
+                .call();
+    }
+
+    private Properties readProperties(final String repo) {
+        // needs local copy of template with credential
+        try (final InputStream stream = getClass().getResourceAsStream(repo + ".properties")) {
+            final Properties properties = new Properties();
+            properties.load(stream);
+            return properties;
+            /* or properties.loadFromXML(...) */
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private Git pullRepository(
+            final File workingDir,
+            final UsernamePasswordCredentialsProvider credProvider
+    ) throws GitAPIException, IOException {
+        final Git git = Git.open(workingDir);
+        git.pull().setProgressMonitor(PROGRESS_MONITOR)
+                .setRebase(true)
+                .setCredentialsProvider(credProvider)
+                .call();
+        return git;
+    }
+
+    private Git cloneRepository(
+            final String gitUrl,
+            final File workingDir,
+            final UsernamePasswordCredentialsProvider credentialsProvider
+    ) throws GitAPIException, IOException {
+        if (workingDir.exists()) {
+            LOGGER.warn("Skipping clone, pulling");
+            return pullRepository(workingDir, credentialsProvider);
+        }
+        return Git.cloneRepository()
+                .setURI(gitUrl)
+                .setDirectory(workingDir)
+                .setProgressMonitor(PROGRESS_MONITOR)
+                .setCredentialsProvider(credentialsProvider)
+                .call();
+    }
+}
---
+++ src/test/java/jgitdemo/LoggerPrintWriter.java
@@ -0,0 +1,50 @@
+package jgitdemo;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+/**
+ * java.io.PrintWriter adapter for org.apache.log4j.Logger.
+ */
+class LoggerPrintWriter extends PrintWriter {
+    public LoggerPrintWriter(final Logger logger, final Level level) {
+        super(new InternalLoggerWriter(logger, level));
+    }
+
+    static class InternalLoggerWriter extends Writer {
+
+        final Logger logger;
+        final Level level;
+
+        InternalLoggerWriter(final Logger logger, final Level level) {
+            this.logger = logger;
+            this.level = level;
+        }
+
+        @Override
+        public void write(char[] cbuf, int off, int len) throws IOException {
+            if (logger.isEnabledFor(level)) {
+                // Remove leading carriage returns inserted by jgit
+                if (cbuf[off] == '\r') {
+                    off++;
+                    len--;
+                }
+                logger.log(level, String.copyValueOf(cbuf, off, len));
+            }
+        }
+
+        @Override
+        public void flush() throws IOException {
+            // no-op
+        }
+
+        @Override
+        public void close() throws IOException {
+            // no-op
+        }
+    }
+}
